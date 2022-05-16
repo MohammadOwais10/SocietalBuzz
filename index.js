3b3dbb30f8e96f37150fe8309f4101214d001909
MohammadOwais10 <owais0945@gmail.com>
2022-05-16 16:55:49 +0530

    Cookie

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+//use cookie parser
+app.use(cookieParser());
 
 //static files access
 app.use(express.static('./assets'));
