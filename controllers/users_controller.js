d4a4bd19d9b9c3a3447f7ee51f9f12ad83049927
MohammadOwais10 <owais0945@gmail.com>
2022-05-16 16:20:47 +0530

    Rendering Pages for SignUp and SignIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (req, res) {
     title: 'User Profile',
   });
 };
+
+// render the sign up page
+module.exports.signUp = function (req, res) {
+  return res.render('user_sign_up', {
+    title: 'Societal | Sign Up',
+  });
+};
+
+// render the sign in page
+module.exports.signIn = function (req, res) {
+  return res.render('user_sign_in', {
+    title: 'Societal | Sign In',
+  });
+};
