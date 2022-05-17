64a2e0635b5c250cae761f59fff52b63c1bda7ee
MohammadOwais10 <owais0945@gmail.com>
2022-05-17 21:52:10 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,21 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-  return res.render('home', {
-    title: 'Home',
-  });
+  //-->here it render but show  only id
+  // Post.find({}, function (err, posts) {
+  //   return res.render('home', {
+  //     title: 'Societal | Home',
+  //     posts: posts,
+  //   });
+  // });
+
+  //populate the user of each post(complete all details render)
+  Post.find({})
+    .populate('user')
+    .exec(function (err, posts) {
+      return res.render('home', {
+        title: 'Societal | Home',
+        posts: posts,
+      });
+    });
 };
