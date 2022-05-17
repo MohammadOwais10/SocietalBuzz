d9d27880a2488bebb5017876c175cd5425060eab
MohammadOwais10 <owais0945@gmail.com>
2022-05-17 16:01:21 +0530

    Fix Bug in Scss

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const MongoStore = require('connect-mongo');
 
 const sassMiddleware = require('node-sass-middleware');
 
-app.use(sassMiddleware)({
-  src: '/assets/scss',
-  dest: '/assets/scss',
-  debug: true,
-  outputStyle: 'extended',
-  prefix: '/css',
-});
+app.use(
+  sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css',
+  })
+);
 
 app.use(express.urlencoded());
 
