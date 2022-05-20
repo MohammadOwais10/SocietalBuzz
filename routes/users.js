63f305c1ad02590c1830acaacfaeae44e4553549
MohammadOwais10 <owais0945@gmail.com>
2022-05-20 22:55:46 +0530

    Google Auth (with Link)

--- routes/users.js
+++ routes/users.js
@@ -28,4 +28,15 @@ router.post(
 
 router.get('/sign-out', usersController.destroySession);
 
+//google oauth
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/users/sign-in' }),
+  usersController.createSession
+);
+
 module.exports = router;
