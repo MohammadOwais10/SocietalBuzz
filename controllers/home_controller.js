efd4a1d6af8afb4d2e7c0b152932be7767d2cab0
MohammadOwais10 <owais0945@gmail.com>
2022-05-22 00:44:21 +0530

    Likes (Functional Work)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,6 +12,7 @@ module.exports.home = async function (req, res) {
 
   //populate the user of each post(complete all details render)
   try {
+    // populate the likes of each post and comment
     let posts = await Post.find({})
       .sort('-createdAt')
       .populate('user')
@@ -20,7 +21,12 @@ module.exports.home = async function (req, res) {
         populate: {
           path: 'user',
         },
-      });
+        populate: {
+          path: 'likes',
+        },
+      })
+      .populate('comments')
+      .populate('likes');
 
     let users = await User.find({});
 
