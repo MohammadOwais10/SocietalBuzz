e2f245c4ce510c600ff344135f2efdc2d48a1b32
MohammadOwais10 <owais0945@gmail.com>
2022-05-22 09:44:41 +0530

    Friendships (Actions and Routes)

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const usersController = require('../controllers/users_controller');
+const friendshipController = require('../controllers/friendship_controller');
 
 router.get(
   '/profile/:id',
@@ -37,6 +38,11 @@ router.get(
   '/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/users/sign-in' }),
   usersController.createSession
+);
+
+router.get(
+  '/profile/:id/toggleFriendship',
+  friendshipController.toggleFriendship
 );
 
 module.exports = router;
