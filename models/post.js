20dec290e62a9c905c6e6b3bae63c1a17e5a8754
MohammadOwais10 <owais0945@gmail.com>
2022-06-02 18:27:02 +0530

    Post Image (Schema and Multer)

--- models/post.js
+++ models/post.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const IMAGE_PATH = path.join('/uploads/users/postimage');
 
 const postSchema = new mongoose.Schema(
   {
     content: {
       type: String,
-      required: true,
+      //required: true,
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
@@ -23,11 +26,39 @@ const postSchema = new mongoose.Schema(
         ref: 'Like',
       },
     ],
+    postImage: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// Multer storage for Post Image
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', IMAGE_PATH));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+postSchema.statics.uploadedImage = multer({
+  //multer settings
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+      return callback(new Error('Only Image are allowed'));
+    }
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+}).single('postImage');
+postSchema.statics.imagePath = IMAGE_PATH;
 
 const Post = mongoose.model('Post', postSchema);
 
