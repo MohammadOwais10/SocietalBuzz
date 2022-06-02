2f8113da5ff589ff5a1d517076c2a7adb308d468
MohammadOwais10 <owais0945@gmail.com>
2022-06-03 01:00:35 +0530

    AJAX Post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,7 +12,7 @@
         data: newPostForm.serialize(),
         success: function (data) {
           let newPost = newPostDom(data.data.post);
-          $('#posts-list-container>ul').prepend(newPost);
+          $('#post-list-container>div').prepend(newPost);
           deletePost($(' .delete-post-button', newPost));
 
           // call the create comment class
@@ -39,45 +39,112 @@
   // method to create a post in DOM
   let newPostDom = function (post) {
     // show the count of zero likes on this post
-    return $(`<li id="post-${post._id}">
-                    <p>
-                        
-                        <small>
-                            <a class="delete-post-button"  href="/posts/destroy/${post._id}">X</a>
-                        </small>
-                       
-                        ${post.content}
-                        <br>
-                        <small>
-                        ${post.user.name}
-                        </small>
-                        <br>
-                        <small>
-                            
-                                <a class="toggle-like-button" data-likes="0" href="/likes/toggle/?id=${post._id}&type=Post">
-                                    0 Likes
-                                </a>
-                            
-                        </small>
+    return $(`
+      <div class="card m-4" id="post-${post._id}">
+      <div class="card-header"  id="post-header" style="padding:0.5rem 1rem ; height:60px">
+          <div class="d-flex flex-row justify-content-between">
 
-                    </p>
-                    <div class="post-comments">
-                        
-                            <form id="post-${post._id}-comments-form" action="/comments/create" method="POST">
-                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
-                                <input type="hidden" name="post" value="${post._id}" >
-                                <input type="submit" value="Add Comment">
-                            </form>
-               
-                
-                        <div class="post-comments-list">
-                            <ul id="post-comments-${post._id}">
-                                
-                            </ul>
-                        </div>
-                    </div>
-                    
-                </li>`);
+              <div class="d-flex flex-row profile-info">
+                  <h3 class="profile-pic-holder" style="width: 45px; height: 45px; margin: 0px 5px;">
+                  ${
+                    post.user.avatar
+                      ? `<img src="${post.user.avatar}" alt="image" style = "width: 100%;height: 100%; border-radius: 50px;"alt="image">`
+                      : `<img src="http://localhost:8000/images/DefaultUser.png" style = "width: 100%;height: 100%; border-radius: 50px;" alt="image">`
+                  }                                           
+                  </h3>
+                  <h4 class="text-capitalize profile-name-holder">
+                      ${post.user.name}
+                  </h4>
+              </div>
+              
+              <div class="dropdown dropleft">
+                  <a class="dropdown-toggle" type="button" id="dropdownMenuButton-${
+                    post._id
+                  }"data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v"></i>
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton-${
+                    post._id
+                  }">     
+                  <a class="delete-post-button dropdown-item" href="/posts/destroy/${
+                    post._id
+                  }">Delete</a>   
+                  </div>
+              </div>
+
+          </div>
+      </div>
+
+      <div class="card-body">
+              ${
+                post.postImage
+                  ? `
+                <div class="posted-image">
+                     <img src="${post.postImage}" alt="post">  
+                </div>
+              `
+                  : `
+              <p>
+                <small>
+                     ${post.content}
+                </small>
+                <br>
+             </p>   
+              `
+              }
+      </div>
+     
+      <div class="card-footer text-muted d-flex flex-row">
+            <div class="mx-2">
+              <a class = "toggle-like-button" data-likes = "${
+                post.likes.length
+              }" href="/likes/toggle/?id=${
+      post._id
+    }&type=Post" style="text-decoration: none;">${
+      post.likes.length
+    } <i class="far fa-thumbs-up"></i>
+            </a>  
+    </div>
+
+          <div class="mx-2">
+                  <button class="btn btn-link btn-block" type="button" data-toggle="collapse" 
+              data-target="#post-${
+                post._id
+              }-collapse" aria-expanded="true" aria-controls="collapseOne" style="width:140px; text-decoration: none; padding-top: 2px;">
+                  <h4 style="font-size: 1rem; margin-left: -20px;">
+                    <i class="fas fa-comment"></i>
+                     Comments
+                  </h4>
+                  </button>
+              </div>    
+          </div>
+
+              <div id="post-${
+                post._id
+              }-collapse" class="collapse card-footer" aria-labelledby="headingOne">
+                  <div class="card-body">
+
+                      <div>
+                        <form action="/comments/create" method="POST" id="post-${
+                          post._id
+                        }-comments-form">
+                         <input type="text" name="content" placeholder="Type here to comment">
+                          <input type="hidden" name="post" value="${post._id}">
+                             <input type="submit" value="Add">
+                          </form>
+                      </div> 
+                                           
+                      <div class="post-comments-list">
+                          <ul id="post-comments-${
+                            post._id
+                          }" style="list-style-type: none; padding: 0px;">                 
+                          </ul>
+                      </div>
+
+                  </div>
+              </div>
+    </div>
+      `);
   };
 
   // method to delete a post from DOM
@@ -107,7 +174,7 @@
 
   // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
   let convertPostsToAjax = function () {
-    $('#posts-list-container>ul>li').each(function () {
+    $('#post-list-container>div>div').each(function () {
       let self = $(this);
       let deleteButton = $(' .delete-post-button', self);
       deletePost(deleteButton);
