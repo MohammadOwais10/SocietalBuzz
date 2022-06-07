f168d74a679b2ff40b8c85f9960266843e19254a
MohammadOwais10 <owais0945@gmail.com>
2022-06-08 01:34:35 +0530

    Morgan and Rotating file stream (Generate logs)

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d',
+  path: logDirectory,
+});
+
 const development = {
   name: 'development',
   asset_path: './assets',
@@ -19,6 +31,11 @@ const development = {
   google_call_back_url: 'http://localhost:8000/users/auth/google/callback',
 
   jwt_secret: 'societal',
+
+  morgan: {
+    mode: 'dev',
+    options: { stream: accessLogStream },
+  },
 };
 
 const production = {
@@ -41,6 +58,11 @@ const production = {
   google_call_back_url: process.env.SOCIETAL_GOOGLE_CALLBACK_URL,
 
   jwt_secret: process.env.SOCIETAL_JWT_SECRET,
+
+  morgan: {
+    mode: 'combined',
+    options: { stream: accessLogStream },
+  },
 };
 
 module.exports =
